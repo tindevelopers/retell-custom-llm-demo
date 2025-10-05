051ab6edd6f9d47d179646168c086eee2683ae33
Gene Foo <gene@Genes-MacBook-Air.local>
2025-10-05 17:00:16 +0200

    Use full path to Python in virtual environment

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Railway startup script
-exec python3 -m uvicorn app.server:app --host 0.0.0.0 --port $PORT
+exec /opt/venv/bin/python -m uvicorn app.server:app --host 0.0.0.0 --port $PORT
